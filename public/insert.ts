559ea6d8156674706f95f6b4a3b17707113dcd17
bombasticSlacks <dillon.matchett@gmail.com>
2024-03-24 21:36:34 -0300

    Formatting

--- public/insert.ts
+++ public/insert.ts
@@ -1,12 +1,15 @@
 import CharacterGenerator from "./generator.js";
 
+const generator = new CharacterGenerator();
+const character = generator.generate();
+
 document.addEventListener("DOMContentLoaded", () => {
-    const generator = new CharacterGenerator();
-    const character = generator.generate();
-    
-    let a = document.querySelector("a");
-    if(a && character.pronouns) {
-        a.text = `${character.name} (${generator.convertPronouns(character.pronouns)})\n A ${character.demeanor} ${character.culture}, who wants to ${character.goal}, and is/has ${character.quirk}`
-        document.body.appendChild(a)
-    }
-})
+  let p = document.querySelector("p");
+  if (p && character.pronouns) {
+    p.textContent = `${character.name} (${generator.convertPronouns(
+      character.pronouns
+    )})\n A ${character.demeanor} ${character.culture}, who wants to ${
+      character.goal
+    }, and is/has ${character.quirk}`;
+  }
+});
